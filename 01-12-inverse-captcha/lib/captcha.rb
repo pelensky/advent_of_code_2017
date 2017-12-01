260e7fb6270d449a4f43ec079ed89116824f59cd
Dan Pelensky <dan@pelensky.com>
2017-12-01 19:08:16 +0000

    second star for day one

--- 01-12-inverse-captcha/lib/captcha.rb
+++ 01-12-inverse-captcha/lib/captcha.rb
@@ -1,17 +1,34 @@
 class Captcha
 
-  def sum(number)
-    find_matching_numbers(number).inject(0, :+)
+  def get_sum_of_matching_numbers_next_to_each_other(number)
+    sum(find_matching_numbers(number))
+  end
+
+  def get_sum_of_far_numbers(number)
+    sum(find_far_numbers(number))
   end
 
   def find_matching_numbers(number)
-    split_numbers = number.to_s.chars.map(&:to_i)
+    split_numbers = split_numbers(number)
     numbers_that_match = number_matches_next(split_numbers)
     first_and_last_numbers_match(split_numbers, numbers_that_match)
     numbers_that_match
   end
 
+  def find_far_numbers(number)
+    split_numbers = split_numbers(number)
+    numbers_that_match = number_matches_half_way(split_numbers)
+  end
+
   private
+
+  def sum(numbers)
+    numbers.inject(0, :+)
+  end
+
+  def split_numbers(number)
+    number.to_s.chars.map(&:to_i)
+  end
 
   def number_matches_next(split_numbers)
     split_numbers.select.each_with_index do |current_number, index|
@@ -22,6 +39,15 @@ class Captcha
   def first_and_last_numbers_match(all_numbers, numbers_that_match)
     if all_numbers.first == all_numbers.last
       numbers_that_match.push(all_numbers.first)
+    end
+  end
+
+  def number_matches_half_way(split_number)
+    index_to_compare = split_number.length/2
+    split_number.select.each_with_index do |current_number, index|
+      index_to_check = index + index_to_compare
+      number_to_check = (index_to_check >= index) ? index - index_to_compare : index_to_check
+      current_number == split_number[number_to_check]
     end
   end
 
