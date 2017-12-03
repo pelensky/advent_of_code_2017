78ed05cc5276aaa2ce467d901c59628804f6012d
Dan Pelensky <dan@pelensky.com>
2017-12-03 00:33:25 +0000

    second exercise of day 2 complete

--- 02-12-corruption-checksum/lib/corruption_checksum.rb
+++ 02-12-corruption-checksum/lib/corruption_checksum.rb
@@ -1,24 +1,43 @@
 class CorruptionChecksum
 
   def compute_sum(input)
-    sum(get_difference(get_values(input)))
+    sum(get_difference(get_sorted_lines(input)))
   end
 
-  def get_values(input)
-    lines = get_lines(input)
+  def compute_division(input)
+    sum(get_divisible_numbers_by_line(get_sorted_lines(input)))
+  end
+
+  private
+
+  def get_divisible_numbers(line)
+    divisible_numbers = []
+    line.each do |divisor|
+      line.select do |quotient|
+        divisible_numbers = put_divisible_numbers_in_array(quotient, divisor, divisible_numbers)
+      end
+    end
+    divisible_numbers.last / divisible_numbers.first
+  end
+
+  def get_divisible_numbers_by_line(lines)
+    lines.map {|line| get_divisible_numbers(line)}
+  end
+
+
+  def put_divisible_numbers_in_array(quotient, divisor, divisible_numbers)
+    if quotient % divisor == 0 && quotient != divisor
+      divisible_numbers.push(divisor)
+      divisible_numbers.push(quotient)
+    end
+    divisible_numbers
   end
 
   def get_difference(lines)
     lines.map {|line| line.last - line.first}
   end
 
-  def sum(values)
-    values.inject(0, :+)
-  end
-
-  private
-
-  def get_lines(input)
+  def get_sorted_lines(input)
     input.split("\n").map do |line|
       get_numbers(line.strip)
     end
@@ -26,6 +45,10 @@ class CorruptionChecksum
 
   def get_numbers(line)
     line.split(" ").map {|number| number.to_i}.sort!
+  end
+
+  def sum(values)
+    values.inject(0, :+)
   end
 
 end
