92090b5f5be2d6e6f1f71fcd7c278365f4d559ad
Dan Pelensky <dan@pelensky.com>
2018-12-08 12:07:57 +0000

    d4 transforms the records

--- 2018/lib/04_response_record.rb
+++ 2018/lib/04_response_record.rb
@@ -8,21 +8,30 @@ class ResponseRecord
       info = record.split(']')[1].strip!
       date = date_and_time.split(' ')[0][1..-1]
       time_string = date_and_time.split(' ')[1]
-      time = convert_time(time_string)
+      converted_date = convert_time(time_string, date)[0]
+      converted_time = convert_time(time_string, date)[1]
 
-      records = add_guard(records, date, time, info) if info.include?('Guard')
-      records = add_sleep_time(records, date, time) if info.include?('asleep')
-      records =add_awake_time(records, date, time) if info.include?('wakes')
+      records = add_guard(records, converted_date, converted_time, info) if info.include?('Guard')
+      records = add_sleep_time(records, converted_date, converted_time) if info.include?('asleep')
+      records = add_awake_time(records, converted_date, converted_time) if info.include?('wakes')
     end
     records
   end
 
   private
 
-  def convert_time(time_stamp)
+  def convert_time(time_stamp, date)
+    split_date = date.split('-')
+    y = split_date[0]
+    m = split_date[1]
+    d = split_date[2].to_i
     hour = time_stamp.split(':')[0].to_i
     minute = time_stamp.split(':')[1].to_i
-    (ONE_HOUR * hour) + minute
+    if hour == 23
+      ["#{y}-#{m}-#{d + 1}", -(60 - minute)]
+    else
+      ["#{y}-#{m}-#{d}", (ONE_HOUR * hour) + minute]
+    end
   end
 
   def add_guard(records, date, time, info)
@@ -37,20 +46,30 @@ class ResponseRecord
   end
 
   def add_sleep_time(records, date, time)
+    records = add_date_entry(records, date)
     if records[date][:asleep]
       records[date][:asleep] << time
     else
       records[date][:asleep] = [time]
     end
+    records[date][:asleep].sort!
     records
   end
 
   def add_awake_time(records, date, time)
+    records = add_date_entry(records, date)
     if records[date][:awake]
       records[date][:awake] << time
     else
       records[date][:awake] = [time]
     end
+    records[date][:awake].sort!
     records
   end
+
+  def add_date_entry(records, date)
+    records[date] = {} unless records[date]
+    records
+  end
+
 end
--- 2018/spec/04_response_record_spec.rb
+++ 2018/spec/04_response_record_spec.rb
@@ -12,14 +12,42 @@ RSpec.describe ResponseRecord do
     result = {
       '1518-03-23' => {
         guard: 2267,
-        begin_shift: 04,
+        begin_shift: 4,
         asleep: [84],
         awake: [146]
       }
     }
-
     expect(response_record.transform(data)).to eq result
+  end
 
+  it 'transforms multipllel dates and times' do
+    data = [
+      '[1518-11-01 00:55] wakes up',
+      '[1518-11-01 00:00] Guard #1 begins shift',
+      '[1518-11-01 00:05] falls asleep',
+      '[1518-11-01 00:30] falls asleep',
+      '[1518-11-01 23:58] Guard #2 begins shift',
+      '[1518-11-02 00:40] falls asleep',
+      '[1518-11-02 00:50] wakes up',
+      '[1518-11-03 00:05] Guard #3 begins shift',
+      '[1518-11-03 00:29] wakes up',
+      '[1518-11-04 00:36] falls asleep',
+      '[1518-11-04 00:46] wakes up',
+      '[1518-11-03 00:24] falls asleep',
+      '[1518-11-05 00:03] Guard #1 begins shift',
+      '[1518-11-05 00:45] falls asleep',
+      '[1518-11-05 00:55] wakes up',
+      '[1518-11-01 00:25] wakes up',
+      '[1518-11-04 00:02] Guard #4 begins shift',
+    ]
+    result = {
+      '1518-11-1' => { asleep: [5, 30], awake: [25, 55], begin_shift: 0, guard: 1 },
+      '1518-11-2' => { asleep: [40], awake: [50], begin_shift: -2, guard: 2 },
+      '1518-11-3' => { asleep: [24], awake: [29], begin_shift: 5, guard: 3 },
+      '1518-11-4' => { asleep: [36], awake: [46], begin_shift: 2, guard: 4 },
+      '1518-11-5' => { asleep: [45], awake: [55], begin_shift: 3, guard: 1 }
+    }
+    expect(response_record.transform(data)).to eq result
   end
 
 end
